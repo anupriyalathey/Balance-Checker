1910d51e0d0f2e87db2202d708177bda7201c016
anupriyalathey <anupriyalathey@gmail.com>
2022-10-23 18:06:13 +0530

    add getBalance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@chakra-ui/react";
-import { ethers } from "ethers";
+import { ethers, BigNumber } from "ethers";
 import Web3Modal from "web3modal";
+import ERC20Abi from "./abis/ERC20.json";
 
 const web3Modal = new Web3Modal({
   providerOptions: {},
@@ -11,6 +12,10 @@ function App() {
   const [instance, setInstance] = useState();
   const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner>();
   const [address, setAddress] = useState<string>();
+  const [daiBalance, setDaiBalance] = useState<string>();
+
+  // Goerli testnet
+  const DAIAddress = "0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844";
 
   // using if inside useEffect will only run when instance is defined
   useEffect(() => {
@@ -30,12 +35,35 @@ function App() {
     getAddress();
   }, [signer]);
 
+  useEffect(() => {
+    const getBalance = async () => {
+      if (signer && address) {
+        const daiContract = new ethers.Contract(DAIAddress, ERC20Abi, signer);
+        const daiBalanceBN = (await daiContract.balanceOf(
+          address
+        )) as BigNumber;
+
+        const daiDecimalsBN = (await daiContract.decimals()) as BigNumber;
+
+        setDaiBalance(ethers.utils.formatUnits(daiBalanceBN, daiDecimalsBN));
+
+        // in single-line
+        // setDaiBalance((await daiContract.balanceOf(address) as BigNumber).toString())
+      }
+    };
+
+    getBalance();
+  }, [address, signer]);
+
   return (
     <>
-      <div>BALANCE CHECKER</div>
+      <div>GOERLI BALANCE CHECKER</div>
       {/* if function is of await use async on arrow function */}
       {address ? (
-        <div>address: {address}</div>
+        <>
+          <div>address: {address}</div>
+          <div>DaiBalance: {daiBalance}</div>
+        </>
       ) : (
         <Button onClick={async () => setInstance(await web3Modal.connect())}>
           Connect Wallet
